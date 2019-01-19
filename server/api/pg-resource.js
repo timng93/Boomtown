68c6a19424f952bd1d52ea2e95dec5c59c842df4
Timmy <timgabrielnguyen@gmail.com>
2019-01-19 12:50:54 -0800

    Insert New Item Query created in pg.resource

--- server/api/pg-resource.js
+++ server/api/pg-resource.js
@@ -57,25 +57,7 @@ module.exports = (postgres) => {
     },
     async getUserById(id) {
 
-      /**
-       *  @TODO: Handling Server Errors
-       *
-       *  Inside of our resource methods we get to determine when and how errors are returned
-       *  to our resolvers using try / catch / throw semantics.
-       *
-       *  Ideally, the errors that we'll throw from our resource should be able to be used by the client
-       *  to display user feedback. This means we'll be catching errors and throwing new ones.
-       *
-       *  Errors thrown from our resource will be captured and returned from our resolvers.
-       *
-       *  This will be the basic logic for this resource method:
-       *  1) Query for the user using the given id. If no user is found throw an error.
-       *  2) If there is an error with the query (500) throw an error.
-       *  3) If the user is found and there are no errors, return only the id, email, fullname, bio fields.
-       *     -- this is important, don't return the password!
-       *
-       *  You'll need to complete the query first before attempting this exercise.
-       */
+    
 
       const findUserQuery = {
         
@@ -85,15 +67,7 @@ module.exports = (postgres) => {
 
       };
 
-      /**
-       *  Refactor the following code using the error handling logic described above.
-       *  When you're done here, ensure all of the resource methods in this file
-       *  include a try catch, and throw appropriate errors.
-       *
-       *  Here is an example throw statement: throw 'User was not found.'
-       *  Customize your throw statements so the message can be used by the client.
-       */
-
+     
        
       try {
         const user = await postgres.query(findUserQuery);
@@ -106,17 +80,7 @@ module.exports = (postgres) => {
      
     async getItems(idToOmit) {
       const items = await postgres.query({
-        /**
-         *  @TODO: Advanced queries
-         *
-         *  Get all Items. If the idToOmit parameter has a value,
-         *  the query should only return Items were the ownerid column
-         *  does not contain the 'idToOmit'
-         *
-         *  Hint: You'll need to use a conditional AND and WHERE clause
-         *  to your query text using string interpolation
-         */
-
+       
         
         text: `SELECT * FROM items ${idToOmit ? 'WHERE ownerid <> $1' : ''}`,
         
@@ -126,10 +90,7 @@ module.exports = (postgres) => {
     },
     async getItemsForUser(id) {
       const items = await postgres.query({
-        /**
-         *  @TODO: Advanced queries
-         *  Get all Items. Hint: You'll need to use a LEFT INNER JOIN among others
-         */
+    
         text: `SELECT * FROM items WHERE ownerid = $1`,
         values: [id]
       });
@@ -137,10 +98,7 @@ module.exports = (postgres) => {
     },
     async getBorrowedItemsForUser(id) {
       const items = await postgres.query({
-        /**
-         *  @TODO: Advanced queries
-         *  Get all Items. Hint: You'll need to use a LEFT INNER JOIN among others
-         */
+     
         text: `SELECT * FROM items WHERE borrowerid = $1`,
         values: [id]
       });
@@ -165,6 +123,8 @@ module.exports = (postgres) => {
       return tags.rows;
     },
     async saveNewItem({ item, image, user }) {
+
+
       /**
        *  @TODO: Adding a New Item
        *
@@ -212,6 +172,13 @@ module.exports = (postgres) => {
                 // Insert new Item
                 // @TODO
                 // -------------------------------
+                const newItemQuery = {
+                  
+                  text: 'INSERT INTO items(title, description, tags) VALUES ($1, $2, $3) RETURNING *',
+                  values: [title, description, tags]
+                };
+               
+              
 
                 const imageUploadQuery = {
                   text:
