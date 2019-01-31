cf7a6eb149535e3303a859f76c28ee0c7a961b36
Timmy <timgabrielnguyen@gmail.com>
2019-01-31 00:24:44 -0800

    Add Markup of User Items and User Profile to Profile Page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import ItemsCard from '../../components/ItemsCard/ItemsCard';
+import Grid from '@material-ui/core/Grid';
 
-const Profile = ({ classes }) => {
+
+
+const Profile = ({ classes, profile}) => {
   return (
     <div>
-      <p>
-        This is the profile page located at <code>/profile/:userId</code>.
-      </p>
+    <h2>{profile.fullname}</h2>
+    <p>  {profile.items.length} Items shared </p>
+    <p> {profile.borrowed.length} Items borrowed</p>
+    <p> {profile.bio} </p>
+    <Grid className= {classes.grid} container spacing={24}>
+    {console.log(profile)}
+    {profile.items.map(item =>{
+      return(
+        <Grid item xs={12} sm={6} md={4} className={classes.gridItem}>
+         <ItemsCard item = {item}/>
+         </Grid>
+      );
+    })}
+    </Grid>
     </div>
+    
   );
 };
 
 export default Profile;
+
+
--- client/src/pages/Profile/ProfileContainer.js
+++ client/src/pages/Profile/ProfileContainer.js
@@ -5,20 +5,24 @@ import styles from './styles';
 import FullScreenLoader from '../../components/FullScreenLoader/FulllScreenLoader';
 import { Query } from 'react-apollo';
 import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
+import NavBar from '../../components/Header/NavBar';
+
 
 class ProfileContainer extends Component {
   render() {
     const id = this.props.match.params.userid || '1';
 
-    return (
+    return (<div>
+      <NavBar />
       <Query variables={{ id: id }} query={ALL_USER_ITEMS_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return <FullScreenLoader inverted />;
           if (error) return <p>{`Error! ${error.message}`}</p>;
           console.log(data);
-          return <Profile classes={this.props.classes} items={data.users} />;
+          return <Profile classes={this.props.classes} profile={data.user} />;
         }}
       </Query>
+      </div>
     );
   }
 }
